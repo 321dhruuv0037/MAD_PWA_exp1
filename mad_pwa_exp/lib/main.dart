ae8e50b1c40658704660fedacf147736b023b0a9
321dhruuv0037 <321dhruuv0037@dbit.in>
2024-03-03 14:05:59 +0530

    Revert "Completed experiment 2"
    This reverts commit b242b2e270faaeb33e8793865e3656b51409d62e.

--- mad_pwa_exp/lib/main.dart
+++ mad_pwa_exp/lib/main.dart
@@ -1,69 +1,99 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      title: 'Experiment1',
+      theme: ThemeData(
+        colorScheme:
+            ColorScheme.fromSeed(seedColor: const Color.fromARGB(255, 0, 0, 0)),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'MAD PWA Experiment 1'),
       debugShowCheckedModeBanner: false,
-      title: 'Experiment 2',
-      home: Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.blueGrey[300],
-          foregroundColor: Colors.white,
-          centerTitle: true,
-          title: const Text(
-            'MAD PWA Experiment 2',
-            style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
-          ),
-        ),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: [
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Image.asset('assets/images/dbit.webp'),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Text(
-                  "Don Bosco Institute of Technology",
-                  style: TextStyle(fontStyle: FontStyle.italic),
-                ),
-              ),
-              const Padding(
-                  padding: EdgeInsets.all(8.0),
-                  child: Row(
-                    children: <Widget>[
-                      Padding(
-                        padding: EdgeInsets.symmetric(horizontal: 8.0),
-                        child: Icon(
-                          Icons.favorite,
-                          color: Colors.red,
-                        ),
-                      ),
-                      Padding(
-                        padding: EdgeInsets.symmetric(horizontal: 8.0),
-                        child: Icon(
-                          Icons.comment_outlined,
-                        ),
-                      ),
-                      Padding(
-                        padding: EdgeInsets.symmetric(horizontal: 8.0),
-                        child: Icon(
-                          Icons.arrow_forward,
-                        ),
-                      ),
-                    ],
-                  )),
-            ],
-          ),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+    ScaffoldMessenger.of(context).showSnackBar(
+      const SnackBar(
+        content: Text('Incremented value!'),
+        duration: Duration(milliseconds: 1000),
+        showCloseIcon: true,
+        backgroundColor: Colors.grey,
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.blueGrey[300],
+        foregroundColor: Colors.white,
+        title: Center(
+          child: Text(widget.title),
         ),
       ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+        backgroundColor: Colors.blueGrey[300],
+        foregroundColor: Colors.white,
+        elevation: 5,
+        splashColor: Colors.black,
+      ),
+      bottomNavigationBar: BottomAppBar(
+        color: Colors.blueGrey[100],
+        child: Container(
+            height: 50.0,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: const <Widget>[
+                Icon(Icons.thumb_up_sharp),
+                Icon(Icons.thumb_down),
+                Icon(Icons.share),
+              ],
+            )),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
